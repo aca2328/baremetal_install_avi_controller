5543f68791aa8f30ef3c309134941b5568c2d08b
Antoine Camerlo <antoine@camerlo.org>
2019-05-22 21:57:19 +0200

    privilege escalation added to ansible-playbook command

--- run.sh
+++ run.sh
@@ -12,4 +12,4 @@ sleep 2
 # set key file to read only
 chmod 600 id_rsa_priv
 # run the container with file mapping and execute the playbook to install docker then the controller on the target host
-docker run -v "$PWD/id_rsa_priv:/ansible/playbooks/id_rsa_priv:ro" -v "$PWD/hosts:/ansible/playbooks/hosts:ro" -v "$PWD/bare_install.yaml:/ansible/playbooks/bare_install.yaml:ro" --rm -ti avibuilder ansible-playbook --private-key=id_rsa_priv -u root -i hosts bare_install.yaml -e "ansible_python_interpreter=/usr/bin/python"
+docker run -v "$PWD/id_rsa_priv:/ansible/playbooks/id_rsa_priv:ro" -v "$PWD/hosts:/ansible/playbooks/hosts:ro" -v "$PWD/bare_install.yaml:/ansible/playbooks/bare_install.yaml:ro" --rm -ti avibuilder ansible-playbook --private-key=id_rsa_priv -b -u root -i hosts bare_install.yaml -e "ansible_python_interpreter=/usr/bin/python"
